087a8ab155236db27646e01300ce913cce8aaa10
Berkay Aycan <berkayaycan07@hotmail.com>
2022-08-09 21:20:54 +0300

    all

--- FindMinimumOfNumber.cpp
+++ FindMinimumOfNumber.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 
 using namespace std;
-void FindMinimum(int x,int y,int z);
+void FindMinimum(int x,int y,int z);//Fonksiyon Imzasi
 
 int main(){
-	FindMinimum(3,5,1);
+	FindMinimum(3,5,1);//Fonksiyonu 3,5,1 sayilari ile çagirma
 	return 0;
 }
-void FindMinimum(int number1,int number2,int number3){
-	int minimum=number1;
-	if(number2<number1)
+void FindMinimum(int number1,int number2,int number3){//Fonksiyonun tanimi
+	int minimum=number1;//Varsayim yontemi ile minimum degiskenine number1 degiskenini atamak.
+	if(number2<number1)//Eger number2, number1 degiskeninden kucukse minimum number2 degiskenidir.
 	 minimum=number2;
-	if(number3<number1)
+	if(number3<number1)//Eger number3, number1 degiskeninden kucukse minimum number2 degiskenidir
 	 minimum=number3;
-	cout<<minimum;
+	cout<<minimum;//minimum degerini ekrana bastir
 }
